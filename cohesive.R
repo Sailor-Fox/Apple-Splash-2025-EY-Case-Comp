994933c5b62bf48eadcd919f095e90d3101609e6
Sailor-Fox <sailorfox2728@outlook.com>
2025-03-28 23:26:52 +1100

    made code cohesive
    but modelling of costs is still ass

---
+++ cohesive.R
@@ -0,0 +1,229 @@
+# doing everything on an industry level. The plots in main.R with all the companies can still go in the appendix though to show the lack of outliers hence assumptions checked.
+library(tidyverse)
+library(readxl)
+library(patchwork)
+data <- read_excel("2025 Case Comp Data CLEAN.xlsx")
+
+# Workforce size by industry ----
+employees_table <- data %>% 
+  select(`Company ID`, Industry, `2023` = `Number of employees 2023`, `2024` = `Number of employees 2024`, `2025` = `Number of employees 2025`) %>% 
+  pivot_longer(cols = 3:5, names_to = "year", values_to = "employees") %>%
+  mutate(year = as.numeric(year)) %>% 
+  group_by(Industry, year) %>% 
+  summarise(employees = sum(employees))
+
+# idk if we want the graph of the straight employees or the sqrt() - thats what the regression was fitted on
+# prolly put employees in main slides and sqrt(employees) in the appendix
+employees_table %>% 
+  ggplot(aes(x = year, y = sqrt(employees), colour = Industry)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    title = "sqrt(Employees) over time by industry",
+    x = "Year",
+    y = "Sqrt(No. of employees)"
+  ) +
+  theme_light()
+
+predict_employees <- function(industry, summary = FALSE, assumption = FALSE) {
+  model <- lm(sqrt(employees) ~ year + Industry, data = employees_table)
+  if(summary) {print(summary(model))}
+  if(assumption) {print(plot(model))}
+  predicted_value <- predict(model, newdata = data.frame(year = 2026, Industry = industry))
+  # result <- list(model = model, prediction = predicted_value)
+  # return(result)
+  return(predicted_value^2)
+}
+
+# Average wage per employee by industry ----
+wages_table <- data %>% 
+  select(`Company ID`, Industry, `2023` = `2023 wages`, `2024` = `2024 wages`, `2025` = `2025 wages`) %>% 
+  pivot_longer(cols = 3:5, names_to = "year", values_to = "wages") %>% 
+  mutate(year = as.numeric(year)) %>% 
+  group_by(Industry, year) %>% 
+  summarise(wages = sum(wages)) %>% 
+  full_join(employees_table) %>% 
+  group_by(Industry, year) %>% 
+  mutate(avg_wages = wages / employees)
+
+wages_table %>% 
+  ggplot(aes(x=year, y=avg_wages, colour = Industry)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    title = "Average wages over time by industry",
+    x = "Year",
+    y = "Average wages"
+  ) +
+  theme_light()
+
+predict_wage <- function(industry, summary = FALSE, assumptions = FALSE) {
+  # input an industry and then output an average wage in 2026
+  model <- lm(avg_wages ~ year + Industry, data = wages_table)
+  if(summary) {print(summary(model))}
+  if(assumptions) {print(plot(model))}
+  predict(model, newdata = data.frame(year = 2026, Industry = industry)) %>% 
+    return()
+}
+
+# Claim rate by industry ----
+claims_table <- data %>% 
+  select(`Company ID`, Industry, `2023` = `2023 number of claims`, `2024` = `2024 number of claims`, `2025` = `2025 number of claims`) %>% 
+  pivot_longer(cols = 3:5, names_to = "year", values_to = "claims") %>% 
+  mutate(year = as.numeric(year)) %>% 
+  group_by(Industry, year) %>% 
+  summarise(claims = sum(claims)) %>% 
+  full_join(employees_table) %>% 
+  group_by(Industry, year) %>% 
+  mutate(claim_rate = sum(claims)/sum(employees))
+
+claims_table %>% 
+  ggplot(aes(x=year, y=claim_rate, colour = Industry)) +
+  geom_line() +
+  labs(title = "Claim rates over time by industry") +
+  theme_light()
+# using a flatline (average of past 3 years) to predict the claims rate
+# the graph seems to suggest that Property and Business Service is decreasing but unsure if this will continue in the future. using the flatline might be overestimating.
+
+predict_claim_rate <- function(industry) {
+  # input an industry and then output an average claim_rate in 2026
+  # regression doesn't seem suitable but an average of the 3 years seems appropiate
+  claims_table %>% 
+    filter(Industry == industry) %>% 
+    pull(claim_rate) %>% 
+    mean() %>% 
+    return()
+}
+
+# NOT COMPLETE AND DODGY RN Average medical cost per claim by industry ----
+  # possibly using survival analysis technique? like KME or whatever
+costs_table <- data %>% 
+  select(Industry, `Company ID`, `2023` = `2023 medical costs`, `2024` = `2024 medical costs`, `2025` = `2025 medical costs`) %>% 
+  pivot_longer(cols = 3:5, names_to = "year", values_to = "costs") %>%
+  mutate(year = as.numeric(year)) %>% 
+  group_by(Industry, year) %>% 
+  summarise(costs = sum(costs)) %>% 
+  full_join(claims_table) %>% 
+  group_by(Industry, year) %>% 
+  mutate(avg_costs = costs / claims) %>% 
+  select(Industry, year, costs, claims, avg_costs)
+
+costs_table %>% 
+  ggplot(aes(x=year, y=avg_costs, colour = Industry)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    title = "Average medical costs over time by industry",
+    x = "Year",
+    y = "Average medical costs"
+  ) +
+  theme_light()
+
+predict_costs <- function(industry) {
+  # input an industry and then output an average medical costs per claim in 2026
+  # THIS IS EXTREMELY PLACE HOLDER RN UNTIL WE ACTUALLY MODEL THE CLAIMS COSTS GOODLY
+  costs_table %>% 
+    filter(Industry == industry) %>% 
+    pull(avg_costs) %>% 
+    mean() %>% 
+    return()
+}
+
+# Make the predictions and add to the original data to graph it ----
+  # add in the appropriate regression line too (as a dashed line of the same colour and no confidence band - too messy)
+
+employees_2026 <- c(
+  predict_employees("Accommodation and Hospitality"),
+  predict_employees("Government Administration"),
+  predict_employees("Fishing and Agriculture"),
+  predict_employees("Education"),
+  predict_employees("Health and Community"),
+  predict_employees("Property and Business Service")
+)
+
+avg_wages_2026 <- c(
+  predict_wage("Accommodation and Hospitality"),
+  predict_wage("Government Administration"),
+  predict_wage("Fishing and Agriculture"),
+  predict_wage("Education"),
+  predict_wage("Health and Community"),
+  predict_wage("Property and Business Service")
+)
+
+claim_rate_2026 <- c(
+  predict_claim_rate("Accommodation and Hospitality"),
+  predict_claim_rate("Government Administration"),
+  predict_claim_rate("Fishing and Agriculture"),
+  predict_claim_rate("Education"),
+  predict_claim_rate("Health and Community"),
+  predict_claim_rate("Property and Business Service") 
+)
+
+avg_costs_2026 <- c(
+  predict_costs("Accommodation and Hospitality"),
+  predict_costs("Government Administration"),
+  predict_costs("Fishing and Agriculture"),
+  predict_costs("Education"),
+  predict_costs("Health and Community"),
+  predict_costs("Property and Business Service")
+)
+
+predicted_data <- employees_table %>% 
+  full_join(wages_table) %>% 
+  full_join(claims_table) %>% 
+  full_join(costs_table) %>% 
+  select(Industry, year, employees, avg_wages, claim_rate, avg_costs) %>% 
+  ungroup() %>% 
+  add_row(Industry = unique(employees_table$Industry),
+          year = 2026,
+          employees = employees_2026,
+          avg_wages = avg_wages_2026,
+          claim_rate = claim_rate_2026,
+          avg_costs = avg_costs_2026)
+
+plot0 <- predicted_data %>% 
+  ggplot(aes(x=year, y=employees, colour = Industry)) +
+  geom_line() +
+  geom_point() + 
+  labs(
+    title = "Employees over time by industry",
+    x = "Year",
+    y = "Employees"   
+  ) +
+  theme_light()
+
+plot1 <- predicted_data %>% 
+  ggplot(aes(x=year, y=avg_wages, colour = Industry)) +
+  geom_line() +
+  geom_point() + 
+  labs(
+    title = "avg_wages over time by industry",
+    x = "Year",
+    y = "avg_wages"   
+  ) +
+  theme_light()
+
+plot2 <- predicted_data %>% 
+  ggplot(aes(x=year, y=claim_rate, colour = Industry)) +
+  geom_line() +
+  geom_point() + 
+  labs(
+    title = "claim_rate over time by industry",
+    x = "Year",
+    y = "claim_rate"   
+  ) +
+  theme_light()
+
+plot3 <- predicted_data %>% 
+  ggplot(aes(x=year, y=avg_costs, colour = Industry)) +
+  geom_line() +
+  geom_point() + 
+  labs(
+    title = "avg_costs over time by industry",
+    subtitle = "cant fully justify the modelling yet",
+    x = "Year",
+    y = "avg_costs"   
+  ) +
+  theme_light()
+
+plot0 + plot1 + plot2 + plot3 + plot_layout(guides = "collect")
--- main.R
+++ main.R
@@ -27,14 +27,8 @@ employees_table <- data %>%
   pivot_longer(cols = 3:5, names_to = "year", values_to = "employees") %>%
   mutate(year = as.numeric(year))
 
-claims_table <- data %>% 
-  select(`Company ID`, Industry, `2023` = `2023 number of claims`, `2024` = `2024 number of claims`, `2025` = `2025 number of claims`) %>% 
-  pivot_longer(cols = 3:5, names_to = "year", values_to = "claims") %>%
-  mutate(year = as.numeric(year))
-
 prepped_data <- full_join(wage_table, cost_table) %>%
-  full_join(employees_table) %>% 
-  full_join(claims_table)
+  full_join(employees_table)
 
 # make a quick visualisation
 company_counts <- prepped_data %>%
@@ -55,7 +49,7 @@ avg_wage_plot <- ggplot(prepped_data, aes(x = year, y = avg_wage)) +
   })) +
   theme_light()
 avg_wage_plot
-# looks like regression split by industry will be appropiate. fairly linear trends and in relatively close bands amongst each industry. No huge outliers anywhere
+# looks like regression split by industry will be appropriate. fairly linear trends and in relatively close bands amongst each industry. No huge outliers anywhere
 
 avg_cost_plot <- ggplot(prepped_data, aes(x = year, y = avg_cost)) +
   geom_line(aes(group = `Company ID`)) +
@@ -65,48 +59,149 @@ avg_cost_plot <- ggplot(prepped_data, aes(x = year, y = avg_cost)) +
     x = "Year",
     y = "Average costs"
   ) +
-  facet_wrap(~Industry) +
+  facet_wrap(~Industry, scale = "free_y") +
   theme_light() +
   geom_smooth(method = "lm")
 avg_cost_plot
 # possibly don't want to split these by industry, they seem similar cost no matter the industry. occasional large outliers indicating regression may be a poor choice. possibly just use a flat line model and then incorporate occasional outliers at a much higher cost (also fishing and agriculture has lots of incidents it seems)
 
-# what I'm aiming to be able to do
-regress_wage <- function(industry, summary = TRUE, assumptions = FALSE) {
-  # input an industry and then output an average wage in 2026
-  model <- lm(avg_wage ~ year, data = filter(avg_table, Industry == industry))
-  if(!summary) {print(summary(model))}
-  if(assumptions) {print(plot(model))}
-  predict(model, newdata = data.frame(year = 2026)) %>% 
+employees_plot <- prepped_data %>% 
+  ggplot(aes(x = year, y = sqrt(employees))) +
+  geom_line(aes(group = `Company ID`)) +
+  geom_point(aes(group = `Company ID`)) +
+  labs(
+    title = "Employees over time by company",
+    x = "Year",
+    y = "Sqrt(No. of employees)"
+  ) +
+  stat_smooth(method = "lm", formula = y ~ x ) +
+  facet_wrap(~Industry, labeller = labeller(Industry = function(x) {
+    paste0(x, "\n(Number of Companies: ", company_counts$n_companies[match(x, company_counts$Industry)], ")")
+  })) +
+  theme_light()
+employees_plot  
+
+
+
+claims_table <- data %>% 
+  select(`Company ID`, Industry, `2023` = `2023 number of claims`, `2024` = `2024 number of claims`, `2025` = `2025 number of claims`) %>% 
+  pivot_longer(cols = 3:5, names_to = "year", values_to = "claims") %>% 
+  mutate(year = as.numeric(year))
+claim_data <- full_join(claims_table, employees_table) %>% 
+  group_by(Industry, year) %>% 
+  summarise(claim_rate = sum(claims)/sum(employees))
+claim_data %>% 
+  ggplot(aes(x=year, y=claim_rate, colour = Industry)) +
+  geom_line() +
+  theme_light()
+# using a flatline (average of past 3 years) to predict the claims rate
+# the graph seems to suggest that Property and Business Service is decreasing but unsure if this will continue in the future. using the flatline might be overestimating.
+
+claim_rate_plot <- full_join(claims_table, employees_table) %>% # a plot by individual companies to show there are minimal outliers - and the ones that are outliers have like 3 employees and had 1 claim. this is why claims were instead looked at on an industry level.
+  ggplot(aes(x=year, y=claims/employees)) +
+  geom_line(aes(group = `Company ID`)) +
+  geom_point(aes(group = `Company ID`)) +
+  labs(
+    title = "Claim rate over time by company and industry",
+    x = "Year",
+    y = "Claim rate"
+  ) +
+  facet_wrap(~Industry, scales = "free_y", labeller = labeller(Industry = function(x) {
+    paste0(x, "\n(Number of Companies: ", company_counts$n_companies[match(x, company_counts$Industry)], ")")
+  })) +
+  theme_light()
+claim_rate_plot
+# to illustrate that it is basically all small companies, lets remove all companies with less than 100 employees (the 1st decile is 101 employees)
+claim_rate_plot_no_small_businesses <- filter(full_join(claims_table, employees_table), employees >= 100) %>% 
+  ggplot(aes(x=year, y=claims/employees)) +
+  geom_line(aes(group = `Company ID`)) +
+  geom_point(aes(group = `Company ID`)) +
+  labs(
+    title = "Claim rate over time by company and industry",
+    x = "Year",
+    y = "Claim rate"
+  ) +
+  facet_wrap(~Industry, scales = "free_y") +
+  theme_light()
+claim_rate_plot_no_small_businesses
+
+predict_claim_rate <- function(industry) {
+  # input an industry and then output an average claim_rate in 2026
+  # regression doesn't seem suitable but an average of the 3 years seems appropiate
+  claim_data %>% 
+    filter(Industry == industry) %>% 
+    pull(claim_rate) %>% 
+    mean() %>% 
     return()
 }
-regress_wage("Education", TRUE)
-regress_wage("Accommodation and Hospitality", TRUE)
-regress_wage("Fishing and Agriculture", TRUE)
-regress_wage("Government Administration", TRUE)
-regress_wage("Health and Community", TRUE)
-regress_wage("Property and Business Service", TRUE)
-# all highly significant (highest p-value is government of 1% but likely due to smallest sample size)
-# R^2 are low but due to limited historical data
+predict_claim_rate("Education")
+predict_claim_rate("Accommodation and Hospitality")
+predict_claim_rate("Fishing and Agriculture")
+predict_claim_rate("Government Administration")
+predict_claim_rate("Health and Community")
+predict_claim_rate("Property and Business Service")
+
+# what I'm aiming to be able to do
+predict_wage <- function(industry, summary = FALSE, assumptions = FALSE) {
+  # input an industry and then output an average wage in 2026
+  model <- lm(avg_wage ~ year + Industry, data = prepped_data)
+  if(summary) {print(summary(model))}
+  if(assumptions) {print(plot(model))}
+  predict(model, newdata = data.frame(year = 2026, Industry = industry)) %>% 
+    return()
+}
+predict_wage("Education")
+predict_wage("Accommodation and Hospitality")
+predict_wage("Fishing and Agriculture")
+predict_wage("Government Administration")
+predict_wage("Health and Community")
+predict_wage("Property and Business Service")
+# significant model with R^2 of 76% - so pretty good
 # the curves in the plot look like they fit well
 
-regress_cost <- function(industry, summary = TRUE) {
+# --------------------------------------------------------------
+predict_cost <- function(industry, summary = TRUE, assumptions = TRUE) {
   # input an industry and then output an average wage in 2026
-  model <- lm(avg_cost ~ year, data = filter(avg_table, Industry == industry))
+  model <- lm(avg_cost ~ year + Industry, data = prepped_data)
   if(summary) {print(summary(model))}
-  predict(model, newdata = data.frame(year = 3)) %>% 
+  if(assumptions) {print(plot(model))}
+  predict(model, newdata = data.frame(year = 2026, Industry = industry)) %>% 
     return()
 }
-regress_cost("Education", TRUE) # NOT SIGNIFICANT
-regress_cost("Accommodation and Hospitality", TRUE) # NOT SIGNIFICANT
-regress_cost("Fishing and Agriculture", TRUE) # NOT SIGNIFICANT
-regress_cost("Government Administration", TRUE) # NOT SIGNIFICANT
-regress_cost("Health and Community", TRUE) # NOT SIGNIFICANT
-regress_cost("Property and Business Service", TRUE) # NOT SIGNIFICANT
-regress_cost(unique(data$Industry), TRUE) # SIGNIFICANT but bad R^2
+predict_cost("Education") # NOT SIGNIFICANT
+predict_cost("Accommodation and Hospitality") # NOT SIGNIFICANT
+predict_cost("Fishing and Agriculture") # NOT SIGNIFICANT
+predict_cost("Government Administration") # NOT SIGNIFICANT
+predict_cost("Health and Community") # NOT SIGNIFICANT
+predict_cost("Property and Business Service") # NOT SIGNIFICANT
+predict_cost(unique(data$Industry)) # SIGNIFICANT but bad R^2
+# --------------------------------------------------------------
+
+employees_by_industry <- prepped_data %>%
+  group_by(Industry, year) %>%
+  summarise(employees = sum(employees))
+employees_by_industry %>%
+  ggplot(aes(x=year, y=(employees)^0.5, colour = Industry)) +
+  geom_line() +
+  theme_light()
+predict_employees <- function(industry, summary = TRUE, assumption = TRUE) {
+  model <- lm(sqrt(employees) ~ year + Industry, data = employees_by_industry)
+  if(summary) {print(summary(model))}
+  if(assumption) {print(plot(model))}
+  predicted_value <- predict(model, newdata = data.frame(year = 2026, Industry = industry))
+  result <- list(model = model, prediction = predicted_value)
+  return(result)
+}
+predict_employees("Education") # 3401.457
+predict_employees("Accommodation and Hospitality") # 2667.659
+predict_employees("Fishing and Agriculture") # 3108.48
+predict_employees("Government Administration") # 2788.309
+predict_employees("Health and Community")
+predict_employees("Property and Business Service")
+# the square root transformation works well. make sure to square the predicted value from the regression to get the actual no. of employees.
 
 # ----
 # TODO
-# regress and predict for employee numbers
+# check what happens if i remove the health and community outlier (does it substantially change results?)
 # work out what to do with medical costs / no. of claims
-  # thinking keep as constant baseline with a certain chacne of the larger outliers.
+  # thinking keep as constant baseline with a certain chance of the larger outliers.
